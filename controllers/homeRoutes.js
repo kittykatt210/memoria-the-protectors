22b973c8290417bbbf5272dca620b62d63b09ecb
N-lara <n_lara01@yahoo.com>
2023-12-02 12:44:52 -0600

    added routes static.js and handlebars for user login, logout, signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,12 +11,17 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // if (req.session.logged_in) {
-  //   res.redirect('/');
-  //   return;
-  // }
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
 
   res.render('login');
 });
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
 
 module.exports = router;
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,17 +1,19 @@
 //TODO replace quearyselector with corresponding name
 
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/login');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector(/*'#logout'*/).addEventListener('click', logout);
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    alert('success');
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
+
   
